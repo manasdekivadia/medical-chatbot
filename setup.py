073c4c81732a9c386d0d9d8d3c5e1b0e15e214a2
manas <manas1729@gmail.com>
2025-06-25 16:03:45 +0300

    Created the basic Layout of the entire Project with all dependencies required.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = "Medical GenAI Chat Bot",
+    version="0.0.0",
+    author = "Manas Dekivada",
+    author_email="manasdekivadia1729@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
